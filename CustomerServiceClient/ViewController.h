8c4a7d946ee6d3885ad8e7bb78398c11bb0c97e0
Bill Zhou <bill@micrometrics.ca>
2015-03-14 12:11:33 -0400

    Initial Commit

---
+++ CustomerServiceClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerServiceClient
+//
+//  Created by Xingnan Zhou on 2015-03-14.
+//  Copyright (c) 2015 SnowWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomerServiceClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomerServiceClient
+//
+//  Created by Xingnan Zhou on 2015-03-14.
+//  Copyright (c) 2015 SnowWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
