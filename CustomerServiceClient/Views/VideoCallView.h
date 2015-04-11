5a7008c372b054e4bc74eae70313ec3ea9088073
Bill Zhou <bill@micrometrics.ca>
2015-04-10 22:47:25 -0400

    Reset Build Paths

--- CustomerServiceClient/Views/VideoCallView.h
+++ CustomerServiceClient/Views/VideoCallView.h
@@ -1,17 +1,17 @@
-////
-////  VideoCallView.h
-////  CustomerServiceClient
-////
-////  Created by Xingnan Zhou on 2015-04-02.
-////  Copyright (c) 2015 SnowWolf. All rights reserved.
-////
 //
-//#import <UIKit/UIKit.h>
-//#import "RTCEAGLVideoView.h"
+//  VideoCallView.h
+//  CustomerServiceClient
 //
-//@interface VideoCallView : UIView
+//  Created by Xingnan Zhou on 2015-04-02.
+//  Copyright (c) 2015 SnowWolf. All rights reserved.
 //
-//@property (nonatomic, readonly) UILabel *statusLabel;
-////@property (nonatomic, readonly) RTCEAGLVideoView *localVideoView;
-////@property (nonatomic, readonly) RTCEAGLVideoView *remoteVideoView;
-//@end
+
+#import <UIKit/UIKit.h>
+#import "RTCEAGLVideoView.h"
+
+@interface VideoCallView : UIView
+
+@property (nonatomic, readonly) UILabel *statusLabel;
+@property (nonatomic, readonly) RTCEAGLVideoView *localVideoView;
+@property (nonatomic, readonly) RTCEAGLVideoView *remoteVideoView;
+@end
